d9508379211b7da4d1ea60e9df1db49b3928ff5c
Stephen Lanszweert <stephenlanszweert@hotmail.be>
2018-11-26 11:08:12 +0100

    Added leaderboard_page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LeaderboardPageComponent } from './components/leaderboard-page/leaderboard-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeaderboardPageComponent
   ],
   imports: [
     BrowserModule,
