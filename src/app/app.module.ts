dbfe5eef1bffaf373c647e0d24d9659be9a40b53
Stephen Lanszweert <stephenlanszweert@hotmail.be>
2018-12-03 13:04:33 +0100

    Fixed leaderboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { LeaderboardPageComponent } from "./components/leaderboard-page/leaderboard-page.component";
+import { LeaderboardTableComponent } from "./components/leaderboard-table/leaderboard-table.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
-  declarations: [AppComponent, LeaderboardPageComponent],
+  declarations: [AppComponent, LeaderboardTableComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
