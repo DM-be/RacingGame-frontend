fbd0eb23a31c2782e062097f38f95f5a06a9d06b
Stephen Lanszweert <stephenlanszweert@hotmail.be>
2018-12-03 11:01:06 +0100

    Added angular material, ready to use it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { MaterialModule } from "./material";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LeaderboardPageComponent } from './components/leaderboard-page/leaderboard-page.component';
-
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { LeaderboardPageComponent } from "./components/leaderboard-page/leaderboard-page.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 @NgModule({
-  declarations: [
-    AppComponent,
-    LeaderboardPageComponent
-  ],
+  declarations: [AppComponent, LeaderboardPageComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
